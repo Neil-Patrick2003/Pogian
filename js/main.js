0fe3cfeb032ae8d11764bb8ddd1fd905587d6869
pogian <delosreyesandrei97@gmail.com>
2025-06-25 16:11:22 +0800

    fixbugno2

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ documment.getElementById("title").innerText = "Welcome";
 document.getElementById("menuBtn").addEventListener("click", toggleMenu);
 
 // toggleMenu is not defined
+
+function toggleMenu(){
+  alert('toggle');
+}
